9963a463b17b401339db051d0cabd201f634e0ed
davesrose <dsr0116@yahoo.com>
2017-08-01 00:52:26 -0400

    adding comments

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,7 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+var mysql = require("mysql"); //npm mysql
+var inquirer = require("inquirer"); //npm inquirer
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection({ //setting new mysql connection from bamazon_DB
   host: "localhost",
   port: 3306,
 
@@ -13,34 +13,35 @@ var connection = mysql.createConnection({
   database: "bamazon_DB"
 });
 
-function viewItems() {
+function viewItems() { //view items function
 	console.log("View Items for Sale");
-	connection.query("SELECT * FROM products", function(err, results) {
+	connection.query("SELECT * FROM products", function(err, results) { //query that calls all products info
 		if(err)throw err;
 		inquirer.prompt([
 		{
 			name: "products",
 			type: "list",
-			choices: function() {
+			choices: function() { //user prompt with list of item's name, price, and quantity
 				var itemArr = [];
 				for (var i=0; i < results.length; i++) {
 					itemArr.push(results[i].product_name+" -$"+results[i].price+" | Quantity: "+results[i].stock_quantity);
 				}
-				itemArr.push("----Exit-----")
+				itemArr.push("----Exit-----") //also push an exit option
 				return itemArr;
 			},
 			message: "Select which product you'd like to see details on"
 		}
 		]).then(function(ans) {
-			if (ans.products === "----Exit-----") {
-				process.exit();
-			} else {
-				var itemName = ans.products.split(' -$')[0];
+			if (ans.products === "----Exit-----") { //if input is exit
+				process.exit(); //exit
+			} else { //else
+				var itemName = ans.products.split(' -$')[0]; //get item name by splicing answers that have -$ after the product name
 				for (var z=0; z < results.length; z++) {
 					if (itemName === results[z].product_name) {
-						var itemDetail = results[z];
+						var itemDetail = results[z]; //create an itemDetail object from the same item name property of the database's results
 					}
 				}
+				//display all details of the item object
 				console.log("ID: "+itemDetail.item_id+" | Name: "+itemDetail.product_name+" | Department: "+itemDetail.department_name+" | Price: $"+itemDetail.price+" | Quantity: "+itemDetail.stock_quantity);
 				inquirer.prompt([
 				{
@@ -48,7 +49,7 @@ function viewItems() {
 					type: "confirm",
 					message: "Do you want to go to the main menu?"
 				}]).then(function(ans) {
-					if (ans.confirm === true) {
+					if (ans.confirm === true) { //confirm prompt for either going back to start (main menu) or exit
 						start();
 					} else {
 						process.exit();
@@ -59,9 +60,9 @@ function viewItems() {
 	});
 };
 
-function lowInventory() {
+function lowInventory() { //function for showing and changing low inventory quantity
 	console.log("View Items that are low in inventory (5 or less)");
-	var query = "SELECT * FROM products WHERE stock_quantity<=5";
+	var query = "SELECT * FROM products WHERE stock_quantity<=5"; //just select products with stock_quantity less then or equal to 5
 	connection.query(query, function(err, results) {
 		if(err)throw err;
 		inquirer.prompt([{
@@ -70,59 +71,60 @@ function lowInventory() {
 			message: "Select which product you'd like to add inventory to",
 			choices: function() {
 				var itemArr = [];
-				for(var i=0; i < results.length; i++) {
+				for(var i=0; i < results.length; i++) { //create loop to display all returned products details
 					itemArr.push(results[i].product_name+" -$"+results[i].price+" | Quantity: "+results[i].stock_quantity);
 				}
-				itemArr.push("----Exit-----");
+				itemArr.push("----Exit-----"); //add option for exit
 				return itemArr;
 			}
 		}]).then(function(ans) {
-			if (ans.products === "----Exit-----") {
+			if (ans.products === "----Exit-----") { //if exit selected, exit
 				process.exit();
 			} else {
-				var itemName = ans.products.split(' -$')[0];
+				var itemName = ans.products.split(' -$')[0]; //else create name variable by splicing selected answer at the: -$ point
 				for (var z=0; z < results.length; z++) {
-					if (itemName === results[z].product_name) {
+					if (itemName === results[z].product_name) { //when the name is equal to the results name, assign it's object
 						var itemDetail = results[z];
 					}
 				}
-				if (itemDetail.stock_quantity >= 0) {
-					console.log("There are "+itemDetail.stock_quantity+" items left.  How many more do you want to buy?");
+				if (itemDetail.stock_quantity >= 0) { //If the item's quantity is greater then 0
+					console.log("There are "+itemDetail.stock_quantity+" items left.  How many more do you want to buy?"); //confirm current quantity
 					inquirer.prompt([{
 						name: "quant",
 						type: "input",
-						message: "Input a number of stock quantity."
+						message: "Input a number of stock quantity." //ask for how much to add
 					}]).then(function(ans) {
-						if (isNaN(ans.quant) === false) {
-							var quant = parseInt(ans.quant)+parseInt(itemDetail.stock_quantity);
+						if (isNaN(ans.quant) === false) { //if it is a number
+							var quant = parseInt(ans.quant)+parseInt(itemDetail.stock_quantity); //add the answered quantity with the stock quantity
+							//run update query to set new quantity to product
 							connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity: quant},{product_name: itemDetail.product_name}], function(err, res) {
 								if(err)throw err;
-								console.log("-----Quantity has been updated-----");
+								console.log("-----Quantity has been updated-----"); //confirm quantity has been updated
 								inquirer.prompt([
 								{
 									name: "confirm",
 									type: "confirm",
-									message: "Do you want to go to the main menu?"
+									message: "Do you want to go to the main menu?" //ask if user wants to return to main menu
 								}]).then(function(ans) {
-									if (ans.confirm === true) {
-										start();
+									if (ans.confirm === true) { //if true
+										start(); //go back to start
 									} else {
-										process.exit();
+										process.exit(); //or exit
 									}
 								})
 							});
-						} else {
-							console.log("You must enter a number");
-							lowInventory();
+						} else { //else answer wasn't number
+							console.log("You must enter a number"); //alert user that they need to enter number
+							lowInventory(); //rerun lowInventory function
 						}
 					});
-				} else {
-					var back = itemDetail.stock_quantity * -1;
-					console.log("There are currently "+back+" stock items backordered.  Be sure to order more then that.")
+				} else { //else stock quantity of item is less then 0 (and have backorders)
+					var back = itemDetail.stock_quantity * -1; //find the remainder backorders
+					console.log("There are currently "+back+" stock items backordered.  Be sure to order more then that.") //display quantity backordered
 					inquirer.prompt([{
 						name: "quant",
 						type: "input",
-						message: "Input a number of stock quantity."
+						message: "Input a number of stock quantity." //
 					}]).then(function(ans) {
 						if (isNaN(ans.quant) === false) {
 							var quant = ans.quant - back;
